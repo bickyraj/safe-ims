d3f933887dbe413a50d9f5e81b926a0b43f48165
bicky raj kayastha <bickyrajkayastha@gmail.com>
2026-06-23 10:52:40 +0200

    initial commit

---
+++ ims-app/src/main/java/com/safepay/ims_app/configurations/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.safepay.ims_app.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+				.cors(AbstractHttpConfigurer::disable)
+				.csrf(CsrfConfigurer::disable)
+				.authorizeHttpRequests(authorize -> {
+					authorize.anyRequest().permitAll();
+				});
+		return http.build();
+	}
+}
---
+++ ims-app/src/main/java/com/safepay/ims_app/controller/AppController.java
@@ -0,0 +1,16 @@
+package com.safepay.ims_app.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+public class AppController {
+
+	@GetMapping
+	public ResponseEntity<String> home() {
+		return ResponseEntity.ok("hello man");
+	}
+}
